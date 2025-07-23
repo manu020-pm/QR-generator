18d12c900ff98abc37bf89b61eb7fd2698b58a94
manu020-pm <manikanthmadiwalar2003@gmail.com>
2025-07-23 15:42:05 +0530

    manikanth

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let qrImage=document.querySelector("#qrImage")
 let qrtext=document.querySelector("#qrtext")
 let btn=document.querySelector(".btn")
 let generateQR=()=>{
+
     if(qrtext.value.length>0){
 qrImage.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+qrtext.value;
 imgbox.classList.add("show-img")
