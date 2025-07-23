3418aa9961f2d2c535c385bbc8e1f8057a566c51
shailesh-benni <shailesh.butter@gmail.com>
2025-07-23 15:22:13 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+let imgbox=document.querySelector("#imgbox")
+let qrImage=document.querySelector("#qrImage")
+let qrtext=document.querySelector("#qrtext")
+let btn=document.querySelector(".btn")
+let generateQR=()=>{
+    if(qrtext.value.length>0){
+qrImage.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+qrtext.value;
+imgbox.classList.add("show-img")
+}else{
+    qrtext.classList.add("error")
+    setTimeout(() => {
+      qrtext.classList.remove("error")  
+    }, 1000);
+}
+    }
+
+btn.addEventListener("click",generateQR)
